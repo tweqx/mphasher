cafa3af75e83e96a73d6242a4e591913a9a3db1e
tweqx <romla@sfr.fr>
2022-05-27 21:02:45 +0000

    Latest changes

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 exit_code=137
-while [ $exit_code == 137 ]
+while [ $exit_code == 137 ] || [ $exit_code == 139 ]
 do
-  # In case the process is killed by SIGKILL (such as when oom-killer triggers), relaunch it
+  # In case the process is killed by SIGKILL (such as when oom-killer triggers)/SIGSEV (when the faulty JH implementation crashes), relaunch it
   python3 -u main.py $1 >> output.txt
 
   exit_code=$?
